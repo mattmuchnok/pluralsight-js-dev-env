f7a93765b83d18fd855760f64df78e0776868701
mattmuchnok <mattmuchnok@gmail.com>
2019-01-16 17:35:22 -0500

    set up ESLint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './index.css';
 
+// This code is just a sample from the 'Building a JavaScript
+// Development Environment' course at pluralsight.com:
+/*
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
+*/
